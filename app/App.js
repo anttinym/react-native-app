3bb59cd89e7f6f4c31d0674fdfc56714f3a73d2a
anttinym <nyman.antti@gmail.com>
2018-05-16 18:27:48 +0300

    first commit, app base added

---
+++ app/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import colors from './constants/colors'
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+    <Text>Changes you make will automatically reload.</Text>
+    <Text>Shake your phone to open the developer menu.</Text>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default App
